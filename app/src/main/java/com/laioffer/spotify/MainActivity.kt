513c30652bd86360df447ec0435518525289b112
hugoyin <zy1265@nyu.edu>
2023-11-23 17:54:00 -0500

    Init

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.laioffer.spotify.ui.theme.SpotifyTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
-class MainActivity : ComponentActivity() {
+// customized extend AppCompatActivity
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "We are at onCreate()");
+
         setContent {
             SpotifyTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +49,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    var name by remember { mutableStateOf("") }
+
+                    // name -> state
+                    // onNameChange -> event
+                    HelloContentWithStateless(name) { typingText ->
+                        name = typingText }
                 }
             }
         }
@@ -30,16 +62,113 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun HelloContentWithState() {
+    var name by remember { mutableStateOf("") }
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello! $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.body2
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun HelloContentWithStateless(name: String, onNameChange: (String) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello! $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.body2
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Name") }
+        )
+    }
+}
+
+
+
+@Composable
+fun ArtistCardBox() {
+    Box {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+fun ArtistCardColumn() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+fun ArtistCardRow() {
+    Row {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCard() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            Column {
+                ArtistCardBox()
+                ArtistCardColumn()
+                ArtistCardRow()
+            }
         }
     }
 }
+
+
+@Composable
+fun Greeting(name: String) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Red)
+        .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello")
+        Text(text = name)
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    SpotifyTheme {
+//        Surface {
+//            Greeting("Android")
+//        }
+//    }
+//}
+
+//@Preview
+//@Composable
+//fun PreviewHelloContent() {
+//    SpotifyTheme {
+//        Surface {
+//            HelloContent()
+//        }
+//    }
+//}
