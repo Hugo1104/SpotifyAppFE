559bd5058c2d433624a1ebecc91829dd3d448c6f
hugoyin <zy1265@nyu.edu>
2023-11-27 21:44:07 -0500

    Image Implementation

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -2,7 +2,6 @@ package com.laioffer.spotify
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
@@ -11,11 +10,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
@@ -28,10 +24,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.ui.theme.SpotifyTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 // customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
@@ -49,15 +47,44 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    var name by remember { mutableStateOf("") }
-
-                    // name -> state
-                    // onNameChange -> event
-                    HelloContentWithStateless(name) { typingText ->
-                        name = typingText }
+//                    var name by remember { mutableStateOf("") }
+//
+//                    // name -> state
+//                    // onNameChange -> event
+//                    HelloContentWithStateless(name) { typingText ->
+//                        name = typingText }
+                    AlbumCover()
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun AlbumCover() {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = "Still Fantasy",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(bottom = 4.dp, start = 2.dp)
+                    .align(Alignment.BottomStart),
+            )
+        }
+
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray,
+        )
     }
 }
 
@@ -150,6 +177,16 @@ fun Greeting(name: String) {
     ) {
         Text(text = "Hello")
         Text(text = name)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 412, heightDp = 732)
+@Composable
+fun DefaultPreview() {
+    SpotifyTheme {
+        Surface {
+            AlbumCover()
+        }
     }
 }
 
