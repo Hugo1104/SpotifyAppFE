984bc3ee5b092cb2419a838c6908b29580a565cd
hugoyin <zy1265@nyu.edu>
2023-12-06 11:43:23 +0800

    Main Page Implementation

--- app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.laioffer.spotify.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,7 +20,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme(colors = darkColors()){
+                    HomeScreen(viewModel)
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,5 +36,3 @@ class HomeFragment : Fragment() {
         }
     }
 }
-
-
